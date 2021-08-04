2af9a4a3fdf55682a4a6b3b78a1d75b00d248df6
Alex Smirnov <sas2job@gmail.com>
2021-08-04 09:26:33 +0300

    Fix test for index

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -2,15 +2,11 @@ require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    books = [
-      {author: "Test Author 1", name: "Test book 1"},
-      {author: "Test Author 2", name: "Test book 2"}
-      ]
     get '/api/v1/books/'
     request_books = JSON.parse(response.body)
     assert_equal(
-      books.count, 
-      request_books.count)
+      request_books.count, Book.count
+    )
     assert_response :success
   end
 end
